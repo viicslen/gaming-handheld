6072bc8d88c38cbe06171f5977f2463b6a493b4d
Victor R <39545521+viicslen@users.noreply.github.com>
2025-10-07 22:14:49 -0400

    Create 1password dir

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,6 +13,7 @@ set -ouex pipefail
 dnf5 remove -y waydroid
 
 # Install 1Password
+mkdir -p /opt/1Password
 rpm --import https://downloads.1password.com/linux/keys/1password.asc
 sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
 dnf5 install -y 1password
