77ef3da5da2a59afe25f2e68548c56df7b077814
Victor R <39545521+viicslen@users.noreply.github.com>
2025-10-07 22:25:54 -0400

    Remove unneeded packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,11 +9,11 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# Remove Waydroid
-dnf5 remove -y waydroid
+# Remove packages not needed
+dnf5 remove -y waydroid webapp-manager sunshine
 
 # Install 1Password
-mkdir -p /opt/1Password
+mkdir /opt/1Password
 rpm --import https://downloads.1password.com/linux/keys/1password.asc
 sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
 dnf5 install -y 1password
